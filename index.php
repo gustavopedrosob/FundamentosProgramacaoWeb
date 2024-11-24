25eb648407b971c4ceeb89b6e83a6db7055be891
Gustavo <thevicio27@gmail.com>
2024-11-24 13:48:25 -0300

    Navbar reutilizada com include

--- index.php
+++ index.php
@@ -13,31 +13,7 @@
         <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
         <div class="drawer-content flex flex-col">
           <!-- Navbar -->
-          <div class="navbar bg-base-300 w-full">
-            <div class="flex-none">
-              <label for="my-drawer-3" aria-label="open sidebar" class="btn btn-square btn-ghost">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  class="inline-block h-6 w-6 stroke-current">
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M4 6h16M4 12h16M4 18h16"></path>
-                </svg>
-              </label>
-            </div>
-            <div class="mx-2 flex-1 px-2">ExploraWeb</div>
-            <div class="hidden flex-none lg:block">
-              <ul class="menu menu-horizontal">
-                <!-- Navbar menu content here -->
-                <li><a>Login</a></li>
-                <li><a>Cadastro</a></li>
-              </ul>
-            </div>
-          </div>
+          <?php include 'navbar.php';?>
           <!-- Page content here -->
           <div class="p-12 space-y-4">
             <p class="font-bold text-4xl underline decoration-emerald-900">Apresentação</p>
@@ -60,9 +36,7 @@
             <p>Explore, Aprenda e Divirta-se!</p>
           </div>
         </div>
-        <?php 
-          include 'sidebar.php';
-        ?>
+        <?php include 'sidebar.php';?>
     </div>
 </body>
 </html>
